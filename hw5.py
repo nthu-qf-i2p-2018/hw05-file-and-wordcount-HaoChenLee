32aabfec7437f5e81a6a5588d90068f39fc41ea3
103070011 <36980232+HaoChenLee@users.noreply.github.com>
2018-04-25 15:00:40 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -54,7 +54,7 @@ def main(filename):
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
     with open('wordcount.pkl', 'wb') as pkl_file:
-        pickle.dump(counter.most_common(), pkl_file)
+        pickle.dump(counter, pkl_file)
 
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
